f988b6890a9c834a8766c248c6ab82bc97b16473
Yashi Bansal <bansalyashi163@gmail.com>
2025-08-29 21:55:32 +0530

    committing navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import {asset} from '../../assets/frontend_assets/asset';
 import {assets} from '../../assets/admin_assets/assets'
@@ -6,17 +6,25 @@ import {assets} from '../../assets/admin_assets/assets'
 
 
 function Navbar() {
+
+  const [menu, setMenu] = useState("App");
+
   return (
-    <div>
+    <div className='navbar'>
       <img src={asset.logo} alt="" className="logo" />
-      <ul className="nav-menu">
-        <li>Home</li>
-        <li>Menu</li>
-        <li>App</li>
-        <li>Contact Us</li>
+      <ul className="navbar-menu">
+        <li onClick={()=>setMenu("Home")} className={menu==="Home"?'active':''}>Home</li>
+        <li onClick={() => setMenu("Menu")} className={menu==="Menu"?'active':''}>Menu</li>
+        <li onClick={() => setMenu("App")} className={menu==="App"?'active':''}>App</li>
+        <li onClick={() => setMenu("Contact-us")} className={menu==="Contact-us"?'active':''}>Contact Us</li>
       </ul>
       <div className="navbar-right">
         <img src={asset.search_icon} alt="Search Icon" />
+        <div className="navbar-search-icon">
+          <img src={asset.basket_icon} alt="" />
+          <div className="dot"></div>
+        </div>
+        <button>Sign In</button>
       </div>
     </div>
   );
