8741de6a2ccc62087344cd2b6ca0c7ca6dcfec71
Yashi Bansal <bansalyashi163@gmail.com>
2025-09-20 22:41:33 +0530

    committing order model

---
+++ backened/controllers/orderController.js
@@ -0,0 +1,11 @@
+import Order from "../models/orderModel.js";
+import User from "../models/userModel.js";
+import stripe from stripe;
+
+
+const placeOrder = async(req,res) => {
+
+}
+
+
+export {placeOrder}
